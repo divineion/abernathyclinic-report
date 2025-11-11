862cb4640b96f7d1f42532a590eb42f8b04d1fa7
divineion <najima.guermoudi@gmail.com>
2025-11-11 16:55:30 +0100

    chore(init): initialize Report microservice with Spring Webflux

---
+++ src/main/java/com/abernathyclinic/report/AbernathyclinicReportApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbernathyclinicReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbernathyclinicReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abernathyclinic/report/AbernathyclinicReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbernathyclinicReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
